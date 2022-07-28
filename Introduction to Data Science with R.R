369b4cdcb874cdac3651cfd5c2bc3d469685855d
Amallysa Jayanti <74315432+amallysajayanti@users.noreply.github.com>
2022-07-28 11:13:29 +0700

    Add files via upload

---
+++ Introduction to Data Science with R.R
@@ -0,0 +1,24 @@
+#Hello world di R.
+"Hello World"
+
+#Perhitungan Sederhana.
+10+7
+
+#variabel dalam R.
+a = 5
+print(a)
+
+10+7 #Ini adalah baris komentar
+
+#Vector pada R 
+c(5,10,20)
+c(1:20)
+
+#Penggunaan Vector bagian satu
+c(3,10,15)
+
+#Penggunaan Vector bagian dua
+c(1:5)
+
+#Menggunakan Fungsi Summary
+summary(c(1:5))
