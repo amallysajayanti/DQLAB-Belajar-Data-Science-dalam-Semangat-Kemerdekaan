e93d75d3577168fa8851067944828c5f6f67979e
Amallysa Jayanti <74315432+amallysajayanti@users.noreply.github.com>
2022-08-26 18:29:34 +0700

    Add files via upload

---
+++ Introduction to Data Science with Python.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Python "Hello World" & Statement
+
+# In[3]:
+
+
+print("Hello World.")
+print("Saya Aksara, baru belajar Python.")
+
+
+# # Variables di Python
+
+# In[5]:
+
+
+bil1 = 10
+Bil_2 = 20
+Frasa = "Halo Dunia"
+bil1, Bil_2 = 10, 20
+salam = "Selamat Pagi"; Penutup = "Salam Sejahtera"
+
+
+# # Comments di Python
+
+# In[6]:
+
+
+#perintah pada baris ini tidak mempengaruhi program
+'''
+perintah ini tidak akan dieksekusi oleh Python
+dan perintah ini juga tidak akan dieksekusi
+perintah ini juga tidak akan dieksekusi
+'''
+print("jadi # digunakan untuk membuat comment pada Python")
+
+
+# # Tugas Praktek
+
+# In[8]:
+
+
+text = "Belajar Python di DQLab."
+print(list(text))
+print(tuple(text))
+print(set(text))
+
+
+# # Menggunakan Library di Python
+
+# In[9]:
+
+
+import math
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+
+# In[ ]:
+
+
+
+
